0b34c6aacfaa9384c1378e6d95b4189bfb3d3b9c
ZoeOliveStraw <zoe.olive.straw@gmail.com>
2024-11-27 19:41:08 -0500

    Debugging LookAround state, not working
    I'm dumb lmao

--- Assets/Scripts/NPC/State_LookAround.cs
+++ Assets/Scripts/NPC/State_LookAround.cs
@@ -7,8 +7,8 @@ public class State_LookAround : State_Abstract
     [SerializeField] private float timeToSeesPlayer;
     [SerializeField] private float rotationSpeed;
 
-    private Vector3 _rightVector;
-    private Vector3 _leftVector;
+    private Vector3 _rightTarget;
+    private Vector3 _leftTarget;
     private Vector3 _targetVector;
     private bool _hasLookedOnce;
     private bool _isWaiting;
@@ -16,14 +16,21 @@ public class State_LookAround : State_Abstract
     
     public override void EnterState(State_Manager manager)
     {
+        _hasLookedOnce = false;
+        _isWaiting = false;
         base.EnterState(manager);
         float coin = Random.Range(0f, 1f);
-        _targetVector = coin < 0.5f ? _rightVector : _leftVector;
         _vision.SetVisionConeColor(visionConeColor);
         _stateManager.timeToSeePlayer = timeToSeesPlayer;
-        _rightVector = transform.right;
-        _leftVector = -transform.right;
+        SetLeftAndrightTarget();
     }
+
+    private void SetLeftAndrightTarget()
+    {
+        Vector3 leftTarget = (-Vector3.right).normalized;
+        Vector3 rightTarget = (Vector3.right).normalized;
+        _targetVector = Random.Range(0,1) < 0.5f ? rightTarget : leftTarget;
+    }   
 
     public override void UpdateState()
     {
@@ -32,35 +39,55 @@ public class State_LookAround : State_Abstract
         {
             _stateManager.SetState(Enum_GuardStates.CanSeePlayer);
         }
-        RotateTowardsTarget();
+        WhatDo();
     }
     
-    private void RotateTowardsTarget()
+    private void WhatDo()
     {
+        float currentAngle = RotateTowardTarget();
+        Debug.LogWarning($"POS: {transform.position}, TARGET: {_targetVector}, ANGLE: {currentAngle}");
         if (_isWaiting)
         {
+            Debug.LogWarning("WAITING");
             _currentWaitTime += Time.deltaTime;
-            if (_currentWaitTime > durationOfCheck)
+            if (_currentWaitTime >= durationOfCheck)
             {
+                _currentWaitTime = 0;
                 _isWaiting = false;
             }
-            else
+        }
+        else if( _hasLookedOnce)
+        {
+            if (currentAngle < 0.1f)
             {
-                return;
+                if(!_hasLookedOnce)
+                {
+                    if(_targetVector == _leftTarget)
+                    {
+                        _targetVector = _rightTarget;
+                    }
+                    else
+                    {
+                        _targetVector = _leftTarget;
+                    }
+
+                    _currentWaitTime = 0;
+                    _isWaiting = true;
+                    _hasLookedOnce = true;
+                }
+                else
+                {
+                    _stateManager.SetState(Enum_GuardStates.Patrol);
+                }
             }
         }
-        
-        if (!_hasLookedOnce)
-        {
-            _hasLookedOnce = true;
-            _targetVector = _targetVector == _rightVector ? _leftVector : _rightVector;
-        }
-        else
-        {
-            _stateManager.SetState(Enum_GuardStates.Patrol);
-            return;
-        }
-        Quaternion targetRotation = Quaternion.LookRotation(_targetVector);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.fixedDeltaTime * rotationSpeed);
+    }
+    
+    private float RotateTowardTarget()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(transform.position - _targetVector);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        float angleToTarget = Quaternion.Angle(transform.rotation, targetRotation);
+        return angleToTarget;
     }
 }
